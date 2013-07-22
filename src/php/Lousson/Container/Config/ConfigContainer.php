72675039e6c8f3feeb8b0a6a2ce981a61b9aaa94
Matze <mhennig@quirkies.org>
2013-07-22 02:08:02 +0200

    Introduce the ConfigContainer class

---
+++ src/php/Lousson/Container/Config/ConfigContainer.php
@@ -0,0 +1,118 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2013, The Lousson Project                               *
+ *                                                                       *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * Redistribution and use in source and binary forms, with or without    *
+ * modification, are permitted provided that the following conditions    *
+ * are met:                                                              *
+ *                                                                       *
+ * 1) Redistributions of source code must retain the above copyright     *
+ *    notice, this list of conditions and the following disclaimer.      *
+ * 2) Redistributions in binary form must reproduce the above copyright  *
+ *    notice, this list of conditions and the following disclaimer in    *
+ *    the documentation and/or other materials provided with the         *
+ *    distribution.                                                      *
+ *                                                                       *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,            *
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)    *
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,   *
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)         *
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED   *
+ * OF THE POSSIBILITY OF SUCH DAMAGE.                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ *  Lousson\Container\Config\ConfigContainer class definition
+ *
+ *  @package    org.lousson.container
+ *  @copyright  (c) 2013, The Lousson Project
+ *  @license    http://opensource.org/licenses/bsd-license.php New BSD License
+ *  @author     Mathias J. Hennig <mhennig at quirkies.org>
+ *  @filesource
+ */
+namespace Lousson\Container\Config;
+
+/** Interfaces: */
+use Lousson\Config\AnyConfig;
+use Lousson\Container\AnyContainer;
+
+/** Dependencies: */
+use Lousson\Container\Generic\GenericContainerEntity;
+
+/**
+ *  A config container implementation
+ *
+ *  @since      lousson/Lousson_Container-0.1.0
+ *  @package    org.lousson.container
+ */
+class ConfigContainer
+    extends GenericContainerEntity
+    implements AnyContainer
+{
+    /**
+     *  Create a container instance
+     *
+     *  The constructor requires the caller to provide a $config instance
+     *  for the container to retrieve option values from.
+     *
+     *  @param  AnyConfig           $config         The container config
+     */
+    public function __construct(AnyConfig $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     *  Obtain a container aggregate
+     *
+     *  The get() method is used to obtain a container aggregate instance
+     *  for the item with the given $name. This aggregate can then get used
+     *  to fetch the actual value of the item.
+     *
+     *  @param  string              $name           The name of the item
+     *
+     *  @return \Lousson\Container\AnyContainerAggregate
+     *          A container aggregate is returend on success
+     *
+     *  @throws \Lousson\Container\AnyContainerException
+     *          Raised in case retrieving the item has failed
+     */
+    public function get($name)
+    {
+        $name = (string) $name;
+        $aggregate = @$this->data[$name];
+
+        if (!isset($aggregate)) {
+            $value = $this->config->getOption($name, null);
+            $aggregate = $this->aggregate($this, $name, $value);
+            $this->data[$name] = $aggregate;
+        }
+
+        return $aggregate;
+    }
+
+    /**
+     *  A cache for aggregates
+     *
+     *  @var array
+     */
+    private $data = array();
+
+    /**
+     *  The container's config
+     *
+     *  @var \Lousson\Config\AnyConfig
+     */
+    private $config;
+}
+
